ba05392a55ed2b67737228e4572f17adb5146431
Sam Davies <sdavies@shinobicontrols.com>
2013-07-29 11:08:58 +0100

    Adds a summary view of the repo at the top of the page
    Currently just displays name, avatar, forks and watchers. We can expand
    this and improve upon the styling. Also not too sure about the loading
    from nib.

--- GithubDashboard/Views/RepoSummaryView.cs
+++ GithubDashboard/Views/RepoSummaryView.cs
@@ -3,6 +3,8 @@ using MonoTouch.UIKit;
 using MonoTouch.Foundation;
 using MonoTouch.ObjCRuntime;
 using System.Drawing;
+using GithubAPI;
+using System.Net;
 
 
 namespace GithubDashboard
@@ -10,17 +12,66 @@ namespace GithubDashboard
 	[Register("RepoSummaryView")]
 	public partial class RepoSummaryView : UIView
 	{
+		private RepoSummaryView _viewFromNib;
+		private RepoSummaryData _repoData;
+
 		public RepoSummaryView (IntPtr h) : base(h)
 		{
 		}
 
-		public RepoSummaryView(NSCoder c) : base(c)
+		public void ChangeRepo(string owner, string repo)
+		{
+			// Ensure that we have already loaded the view from the layout nib
+			if(_viewFromNib == null) {
+				this.LoadFromNib ();
+			}
+			// Update the repo data
+			_repoData = RepoSummary.SummmaryForRepo (owner, repo);
+			// Refresh the view
+			this.UpdateViewForRepoData ();
+		}
+
+
+		#region Utility Methods
+		private void UpdateViewForRepoData()
+		{
+			// Set the repo name
+			_viewFromNib.repoName.Text = _repoData.owner.login + " / " + _repoData.name;
+			// Set the number of forks
+			_viewFromNib.forks.Text = "Forks: " + _repoData.forks.ToString ();
+			// Set the number of watchers
+			_viewFromNib.watchers.Text = "Watchers: " + _repoData.watchers.ToString ();
+			// Update the owner's image
+			this.UpdateOwnerImage (_repoData.owner.avatar_url);
+		}
+
+		private void UpdateOwnerImage(string url)
+		{
+			var webClient = new WebClient ();
+			webClient.DownloadDataCompleted += (sender, e) => 
+			{
+				UIImage image = this.GetImageFromByteArray(e.Result);
+				InvokeOnMainThread (() => {
+					_viewFromNib.ownerImage.Image = image;
+				});
+			};
+			webClient.DownloadDataAsync(new Uri(url));
+		}
+
+		private UIImage GetImageFromByteArray (byte[] imageBuffer)
+		{
+			NSData imageData = NSData.FromArray (imageBuffer);
+			return UIImage.LoadFromData (imageData);
+		}
+
+		private void LoadFromNib()
 		{
 			var arr = NSBundle.MainBundle.LoadNib("RepoSummaryView", this, null);
-			var v = Runtime.GetNSObject(arr.ValueAt(0)) as UIView;
-			v.Frame = new RectangleF(0, 0, Frame.Width, Frame.Height);
-			AddSubview(v);
+			_viewFromNib = Runtime.GetNSObject(arr.ValueAt(0)) as RepoSummaryView;
+			_viewFromNib.Frame = new RectangleF(0, 0, Frame.Width, Frame.Height);
+			AddSubview(_viewFromNib);
 		}
+		#endregion
 	}
 }
 

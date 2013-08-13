1c5dff1670a15bf51bbab8b447e83c9ef22cb0fc
Sam Davies <sdavies@shinobicontrols.com>
2013-08-13 08:59:37 +0100

    Adds authentication for GitHub requests
    The token should be provided in a JSON file - a sample of which is
    present in this commit. Instructions are provided inside the
    GithubDataProvider class.
    
    Have noticed a null response bug when changing repo. Will investigate/fix
    this for the next commit.

--- GithubDashboard/GithubAPI/GithubDataProvider.cs
+++ GithubDashboard/GithubAPI/GithubDataProvider.cs
@@ -2,12 +2,15 @@ using System;
 using System.Collections.Generic;
 using RestSharp;
 using System.Linq;
+using Newtonsoft.Json.Linq;
+using System.IO;
 
 namespace GithubAPI
 {
 	static public class GithubDataProvider
 	{	
 		static private String BaseURL = "https://api.github.com/";
+		static private string authTokenJsonPath = "./GithubAPI/GithubAuthenticationToken.json";
 
 		#region API Methods to pull different data from the github API
 		public static void WeeklyCommitForRepo(string owner, string repo, Action<WeeklyCommitData> callback)
@@ -33,11 +36,13 @@ namespace GithubAPI
 			// Perform an async request call. Send the data back to the caller
 			client.ExecuteAsync<List<List<long>>> (request, response => {
 				// Let's convert the 2D array into an 'array' of PunchCardEntry objects
-				var mapped =
-					from dp in response.Data
-						select new CodeFrequencyEntry (dp);
-				// And push the results back
-				callback(mapped);
+				if(response.Data != null) {
+					var mapped =
+						from dp in response.Data
+							select new CodeFrequencyEntry (dp);
+					// And push the results back
+					callback(mapped);
+				}
 			});
 		}
 
@@ -93,7 +98,44 @@ namespace GithubAPI
 		// This method could be replaced with some IoC magic
 		static private IRestClient GetGithubRestClient()
 		{
-			return new RestClient (BaseURL);
+			var client = new RestClient (BaseURL);
+
+			// Let's see whether we have some authentication details
+			if(File.Exists (authTokenJsonPath)) {
+
+				/* Without authentication GitHub limits your IP address to 60 requests per
+				 * hour. This is a problem since changing the repo in the dashboard app requires
+				 * at least 5 requests.
+				 * Authenticating against github enables you to perform up to 5000 requests per
+				 * hour. Therefore we check here whether or not credentials have been provided
+				 * and if they have then we'll use them.
+				 * 
+				 * To create credentials for the app
+				 * (based on https://help.github.com/articles/creating-an-access-token-for-command-line-use):
+				 * 1. Visit https://github.com/settings/applications
+				 * 2. Click create new token
+				 * 3. Give it an appropriate name, and copy the generated token.
+				 * 4. Using GithubAuthenticationToken.sample.json as a template create
+				 *    GithubAuthenticationToken.json - with your username and token pasted in the
+				 *    appropriate places.
+				 * 5. Right click on the JSON file in Xamarin Studio, and ensure that under build
+				 *    action "BundleResource" is selected.
+				 * 6. Ensure that you don't commit the JSON file into source control.
+				 * 
+				 * Please note that this solution is not suitable for production applications - for
+				 * these you should implement a user-based OAuth2 solution, as per the instructions
+				 * on GitHub
+				 */
+
+				var parsedObjects = JObject.Parse (File.ReadAllText (authTokenJsonPath));
+				string username = (string)parsedObjects["personal_access_token"]["user"];
+				string token = (string)parsedObjects["personal_access_token"]["token"];
+				if (!String.IsNullOrWhiteSpace (username) && !String.IsNullOrWhiteSpace (token)) {
+					// We have credentials, so let's add them to the client
+					client.Authenticator = new HttpBasicAuthenticator (username, token);
+				}
+			}
+			return client;
 		}
 
 		static private IRestRequest GetGithubRestRequest(String urlPart, String owner, String repo)
